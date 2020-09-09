4059f86e46ccd68dc4bc6ec08ed98bc204fbcecd
ushantharyan55 <70165516+ushantharyan55@users.noreply.github.com>
2020-09-09 21:40:25 +0530

    Update recursion.js

--- src/lab/exp9/simulation/JS/recursion.js
+++ src/lab/exp9/simulation/JS/recursion.js
@@ -19,12 +19,10 @@ window.view = {
 	},
 	getInput: function() {
 		 var input = Number(document.getElementById('input').value)
-		 if ( input === 0 )
-		 	alert('Enter number of disks first !')
+		  if ( input < 0 && input > 10 )
+			alert('Invaild Input')
 		 else if ( isNaN(input) === true )
 			alert('Number of disks must be an integer value !')
-		 else if ( input > 5 || input % 1 !== 0 )
-			alert('Only integral value (Greater than equal to 1 and less than equal to 5) is accepted !')
 		 else {
 			this.disableElement('btnOk')
 			this.enableElement('btnStart')
@@ -290,4 +288,4 @@ window.view = {
 		this.activateEvents()
 	}
 }
-window.onload = function() { view.init() }																										
+window.onload = function() { view.init() }
